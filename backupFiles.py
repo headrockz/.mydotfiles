7c4b5cae4bf0f71f3d9cf046668b093ef5cddca9
Asafe Felipe <azapphrz@gmail.com>
2022-02-11 22:17:16 -0300

    update 2022-02-11 MacOS

--- backupFiles.py
+++ backupFiles.py
@@ -3,34 +3,31 @@ import platform
 import os
 
 """
-I use two different operating systems, Linux and MacOS, 
-basically the dotfiles are the same, but there may be differences in each one, 
-so it is necessary to check which system the script is running on
+I use two different operating systems, Linux and macOS, basically the dotfiles are the same, but there may be differences in each one, so it is necessary to check which system the script is running on
 """
+
+date = date.today()
 
 if (platform.system() == 'Linux'):
 
     # copy my dotfiles to repository folder
     os.system('cat ~/.zshrc > .zshrc')
 
-    date = date.today()
-
-    # print(platform.system())
     #commit the changes
     os.system('echo "Commit dotfiles"')
     os.system(f'git commit -am "update {date}"')
     os.system('git push')
     os.system('echo "Have a nice day!"')
 
-
 else:
     # copy my dotfiles to repository folder
     os.system('cat ~/.zshrc > ./macOS/.zshrc')
 
-    date = date.today()
 
-    # print(platform.system())
+
     #commit the changes
-    os.system("echo mac")
-    # os.system(f'git commit -am "update {date}"')
-    # os.system('git push')
+    os.system('git add .')
+    os.system('echo "Commit dotfiles"')
+    os.system(f'git commit -am "update {date} MacOS"')
+    os.system('git push')
+    os.system('echo "Have a nice day!"')
